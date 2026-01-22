4e5a6c9f5fd3ad737dd6d6cbe89d183214bc598f
pandeynitin-5532 <nitinpandey5532@gmail.com>
2026-01-22 17:26:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (87.43%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,9 +1,7 @@
 class MyStack {
     queue<int> q;
-
 public:
     MyStack() {}
-
     void push(int x) {
         q.push(x);
         int sz = q.size();
@@ -12,17 +10,14 @@ public:
             q.pop();
         }
     }
-
     int pop() {
         int val = q.front();
         q.pop();
         return val;
     }
-
     int top() {
         return q.front();
     }
-
     bool empty() {
         return q.empty();
     }
