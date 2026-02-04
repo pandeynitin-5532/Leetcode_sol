fc21493996983b369a31ba9a4a9fb06fe4e4e251
pandeynitin-5532 <nitinpandey5532@gmail.com>
2026-02-04 20:30:11 +0530

    Time: 27 ms (57.55%) | Memory: 27.6 MB (29.47%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -14,21 +14,6 @@ public:
                 }
             }
         }
-        int i=m , j=n;
-        string lcs="";
-        while(i>0 && j>0){
-            if(text1[i-1]==text2[j-1]){
-                lcs+=text1[i-1];
-                i--;
-                j--;
-            }
-            else if(dp[i-1][j]>dp[i][j-1]){
-                i--;
-            }
-            else {
-                j--;
-            }
-        }
-        return lcs.length();
+        return dp[m][n];
     }
 };
