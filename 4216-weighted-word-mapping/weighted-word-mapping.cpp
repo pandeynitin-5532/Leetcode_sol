f4bb3ca54638a7c7426a55359e7affb6c38f12dd
pandeynitin-5532 <nitinpandey5532@gmail.com>
2026-06-13 20:30:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (77.60%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+
+        for (auto& w : words) {
+            int s = 0;
+
+            for (char c : w) {
+                s = (s + weights[c - 'a']) % 26;
+            }
+
+            ans.push_back('a' + (25 - s));
+        }
+
+        return ans;
+    }
+};
